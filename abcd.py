829e8b057bea2b1ed0a78fc6d00fbb0b69c52119
Akshat-Kushwaha104 <akshatkushwaha17@gmail.com>
2025-08-18 14:26:45 +0530

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -1,15 +1,19 @@
-n = 5 
-for i in range(n):
-    
-    for j in range(n - i):
-        print(chr(65 + j), end=" ")
-    
-    print("  " * (2 * i - 1), end="")
-    
-    
-    for j in range(n - i - 1, -1, -1):
-        if not (i > 0 and j == n - i - 1):
-            print(chr(65 + j), end=" ")
-    
-    print()
+def print_pattern():
+    rows = 5
+    for i in range(rows):
+        
+        for j in range(rows - i):
+            print(chr(65 + j), end=' ')
+        
+        
+        spaces = 2 * i - 1
+        print('  ' * spaces, end='')
 
+        
+        for j in reversed(range(rows - i)):
+            if not (i == 0 and j == rows - i - 1):  
+                print(chr(65 + j), end=' ')
+        
+        print()
+
+print_pattern()
